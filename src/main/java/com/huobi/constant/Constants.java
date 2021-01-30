3f900ca46046bbd109fab2529626f7e5d1471bfa
bossrin <33567842+bossrin@users.noreply.github.com>
2021-01-31 00:12:21 +0700

    Update Constants.java

--- src/main/java/com/huobi/constant/Constants.java
+++ src/main/java/com/huobi/constant/Constants.java
@@ -2,7 +2,7 @@ package com.huobi.constant;
 
 public class Constants {
 
-  public static final String API_KEY = "AccessKey";
-  public static final String SECRET_KEY = "SecretKey";
+  public static final String API_KEY;
+  public static final String SECRET_KEY;
 
 }
